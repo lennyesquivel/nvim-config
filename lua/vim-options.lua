6fec4f2eb87ed30f745de111790ea07ba4ffd331
Lenny Esquivel <lenny.esquivel.fs@gmail.com>
2025-07-13 23:34:21 -0600

    working main plugins and config

---
+++ lua/vim-options.lua
@@ -0,0 +1,10 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
